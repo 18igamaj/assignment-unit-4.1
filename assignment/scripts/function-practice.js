95ab1dc4295b18a577408ff27fde74065ae627b4
Majid Igale <majidigale18@gmail.com>
2023-04-23 08:01:54 -0500

    Coded question 7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -81,7 +81,16 @@ console.log(isPositive(-10000000000));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
-function find(value, array) {}
+function find(value, array) {
+  for(i=0; i < array.length; i++){
+    if(array[i]===value ){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
 
 // ----------------------
 // Stretch Goals
